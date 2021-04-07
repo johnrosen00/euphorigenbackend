f7a56052f434a40c2ada507bec2eea73590f1b11
John Rosen <johnrosen00@gmail.com>
2021-04-07 16:44:19 -0700

    got shell script working again

--- servers/gateway/test-deploy.sh
+++ servers/gateway/test-deploy.sh
@@ -11,6 +11,7 @@ docker rm -f eugateway
 docker pull johnrosen00/eugateway
 
 
+
 export TLSKEY="/etc/letsencrypt/live/api.johnrosen.me/privkey.pem"
 export TLSCERT="/etc/letsencrypt/live/api.johnrosen.me/fullchain.pem"
 
@@ -18,6 +19,14 @@ export TLSCERT="/etc/letsencrypt/live/api.johnrosen.me/fullchain.pem"
 export REDISADDR="redisServer:6379"
 export DSN="root:test@tcp(eudb:3306)/store"
 export SESSIONKEY="bnottomtext"
-export MANPASS="eleven"
-#docker run -d -p 443:443 --net api -v /etc/letsencrypt:/etc/letsencrypt:ro -e REDISADDR=$REDISADDR -e DSN=$DSN -e SESSIONKEY=$SESSIONKEY -e TLSKEY=$TLSKEY -e TLSCERT=$TLSCERT --name eugateway johnrosen00/eugateway
-docker run -d -p 443:443 --net api -e REDISADDR=$REDISADDR -e MANPASS=$MANPASS -e DSN=$DSN -e SESSIONKEY=$SESSIONKEY -e TLSKEY=$TLSKEY -e TLSCERT=$TLSCERT --name eugateway johnrosen00/eugateway
+export MANPASS="CalvinGarfieldWilson"
+
+docker run -d -p 443:443 --net api \
+-e REDISADDR=$REDISADDR \
+-e MANPASS=$MANPASS \
+-e DSN=$DSN \
+-e SESSIONKEY=$SESSIONKEY \
+-e TLSKEY=$TLSKEY \
+-e TLSCERT=$TLSCERT \
+--name "eugateway" \
+johnrosen00/eugateway
